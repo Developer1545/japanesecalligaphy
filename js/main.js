dca2ae055002b70f7ff400a3016731b5f5f58d4d
Your Name <Developer1545>
2020-10-14 11:06:54 -0400

    Added clickable Functionality for sounds

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   
+})
+function hoverBlock(block) {
+    console.log(block);
+
+    switch(parseInt(block)) {
+        case 1:
+            console.log("playing Sound 1");
+            break;
+        case 2:
+            console.log("playing Sound 2");
+            break;
+
+    }
+}
