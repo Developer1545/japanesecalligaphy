84de4149a7f83335b3c0710e5ea5cc34fd3da3b3
Your Name <Developer1545>
2020-10-18 18:22:29 -0400

    Added sounds
    Need one more word and sound and then need to add styling!

--- js/main.js
+++ js/main.js
@@ -6,10 +6,43 @@ function hoverBlock(block) {
 
     switch(parseInt(block)) {
         case 1:
-            console.log("playing Sound 1");
+            var japan = new Audio('./soundfiles/japan.mp3');
+            japan.play();
             break;
         case 2:
-            console.log("playing Sound 2");
+            var peaceful = new Audio('./soundfiles/peaceful.mp3');
+            peaceful.play();
+            break;
+        case 3:
+            var fire = new Audio('./soundfiles/fire.mp3');
+            fire.play();
+            break;
+        case 4:
+            var tree = new Audio('./soundfiles/tree.mp3');
+            tree.play();
+            break;
+        case 5:
+            var mountain = new Audio('./soundfiles/mountain.mp3');
+            mountain.play();
+            break;
+        case 6:
+            var america = new Audio('./soundfiles/america.mp3');
+            america.play();
+            break;
+        case 7:
+            var friendship = new Audio('./soundfiles/friendship.mp3');
+            friendship.play();
+            break;
+        case 8:
+            var daoism = new Audio('./soundfiles/daoism.mp3');
+            daoism.play();
+            break;
+        case 9:
+            var sushi = new Audio('./soundfiles/sushi.mp3');
+            sushi.play();
+            break;
+        default:
+            console.log("Sound not found!");
             break;
 
     }
